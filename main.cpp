c6da69b337ce38c7b82ef752eb6a29e6c4b6008f
Chris Jenkins <cdpjenkins@gmail.com>
2025-02-02 14:52:47 +0000

    Chapter 2: parsing doubles and writing simple tests using assert()

--- main.cpp
+++ main.cpp
@@ -1,5 +1,35 @@
-#include <print>
+// #include <print>
+#include <cassert>
+#include <istream>
+#include <sstream>
+
+[[nodiscard]]
+bool get_number(std::istream & input_stream, double & number) {
+    input_stream >> number;
+
+    if (input_stream) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void test_code() {
+
+    double value{};
+
+    std::stringstream some_input{"1"};
+    const bool ok = get_number(some_input, value);
+    assert(ok);
+    assert(value == 1);
+
+    double unused{};
+    std::stringstream bad_input{"q"};
+    const bool not_ok = get_number(bad_input, unused);
+    assert(!not_ok);
+}
+
 int main()
 {
-    std::println("Hello, World!");
+    test_code();
 }
