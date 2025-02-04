75c7e4995f1aba733713a682b3043c302b6f8fd2
Chris Jenkins <cdpjenkins@gmail.com>
2025-02-04 20:16:40 +0000

    Reading numbers into an array in a while loop

--- loops.cpp
+++ loops.cpp
@@ -1,29 +1,38 @@
 #include "loops.hpp"
 
+#include <array>
 #include <expected>
 #include <iostream>
 
-static std::expected<double, std::string> get_number(std::istream & cin) {
+static std::expected<double, std::string> get_number(std::istream & input_stream) {
     double number{};
 
-    cin >> number;
-    if (cin) {
+    input_stream >> number;
+    if (input_stream) {
         return number;
     }
+    input_stream.clear();
+    input_stream.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     return std::unexpected("That's not a number");
 }
 
 void loop_main() {
-    std::cout << "Please enter a number.\n> ";
-    auto number = get_number(std::cin);
 
-    while (number.has_value()) {
-        std::cout << "Got " << number.value() << "\n";
+    std::array<double, 5u> numbers{};
 
-        std::cout << "Please enter another number.\n> ";
-        number = get_number(std::cin);
+    std::cout << "Please enter some numbers.\n> ";
+    size_t count = 0;
+    while (count < numbers.size())
+    {
+        auto number = get_number(std::cin);
+        if (number.has_value()) {
+            numbers[count] = number.value();
+            std::cout << "Got " << number.value() << "\n";
+        } else {
+            std::cout << number.error() << std::endl;
+        }
+
+        ++count;
     }
-
-    std::cout << number.error() << std::endl;
 }
