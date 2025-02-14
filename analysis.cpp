97aab9b2e4fd395b8b030c027d6362d20be15b7c
Chris Jenkins <cdpjenkins@gmail.com>
2025-02-14 17:39:17 +0000

    Horribly wrong average function that always returns 0.0

--- analysis.cpp
+++ analysis.cpp
@@ -1,2 +1,7 @@
 #include "analysis.hpp"
 
+#include <vector>
+
+double average(const std::vector<double>& prices) {
+    return 0.0;
+}
--- analysis.hpp
+++ analysis.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <vector>
+
 namespace stock_prices {
     inline bool negative(double value) {
         return value < 0.0;
     }
+
+    double average(const std::vector<double>& prices)
 }
